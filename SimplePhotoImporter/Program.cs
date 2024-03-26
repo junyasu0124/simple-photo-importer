525064117b3dbaeec0bf35f2389c55a3a15d8805
junyasu <jumpeiyasue@gmail.com>
2024-03-26 22:47:04 +0900

    feat: Adding methods to get files by separating mode

--- SimplePhotoImporter/Program.cs
+++ SimplePhotoImporter/Program.cs
@@ -26,7 +26,7 @@ if (shell == null)
   return;
 }
 
-string fromPath;
+string sourcePath;
 while (true)
 {
   Console.WriteLine("Enter the path to the folder you want to import from:");
@@ -36,12 +36,12 @@ while (true)
     Console.WriteLine("The specified path does not exist.");
   else
   {
-    fromPath = inputFromPath;
+    sourcePath = inputFromPath;
     break;
   }
 }
 
-string toPath;
+string destPath;
 while (true)
 {
   Console.WriteLine("Enter the path to the folder you want to import to:");
@@ -51,7 +51,7 @@ while (true)
     Console.WriteLine("The specified path does not exist.");
   else
   {
-    toPath = inputToPath;
+    destPath = inputToPath;
     break;
   }
 }
@@ -59,26 +59,90 @@ while (true)
 SeparateMode separateMode;
 while (true)
 {
-  Console.WriteLine("Do you want to separate files by date? (y/n)");
+  Console.WriteLine("Enter the mode to separate the files:");
+  Console.WriteLine("1: By year, month, and day");
+  Console.WriteLine("2: By day");
+  Console.WriteLine("3: By none");
   var inputSeparateMode = Console.ReadLine();
 
-  if (inputSeparateMode == "y" || inputSeparateMode == "Y")
+  if (inputSeparateMode == "1")
   {
-    separateMode = SeparateMode.ByDate;
+    separateMode = SeparateMode.ByYearAndMonthAndDay;
     break;
   }
-  else if (inputSeparateMode == "n" || inputSeparateMode == "N")
+  else if (inputSeparateMode == "2")
+  {
+    separateMode = SeparateMode.ByDay;
+    break;
+  }
+  else if (inputSeparateMode == "3")
   {
     separateMode = SeparateMode.ByNone;
     break;
+  }
+  else if (inputSeparateMode == null)
+  {
+    continue;
+  }
+  else
+  {
+    Console.WriteLine("Invalid input.");
   }
 }
 
 
 (Shell32.Folder folder, string? path)? folder = null;
+if (separateMode == SeparateMode.ByNone)
+{
+  var files = GetFiles(sourcePath);
+  foreach (var file in files)
+  {
+    var fileName = Path.GetFileName(file.Key);
+    var newFilePath = Path.Combine(destPath, fileName);
+    File.Move(file.Key, newFilePath);
+  }
+}
+else if (separateMode == SeparateMode.ByYearAndMonthAndDay)
+{
+  var files = GetFilesSeparatedByYearAndMonthAndDay(sourcePath);
+  foreach (var year in files)
+  {
+    foreach (var month in year.Value)
+    {
+      foreach (var day in month.Value)
+      {
+        var directoryPath = Path.Combine(destPath, year.Key.ToString(), month.Key.ToString(), day.Key.ToString());
+        Directory.CreateDirectory(directoryPath);
+
+        foreach (var (path, date) in day.Value)
+        {
+          var fileName = Path.GetFileName(path);
+          var newFilePath = Path.Combine(directoryPath, fileName);
+          File.Copy(path, newFilePath);
+        }
+      }
+    }
+  }
+}
+else if (separateMode == SeparateMode.ByDay)
+{
+  var files = GetFilesSeparatedByDay(sourcePath);
+  foreach (var day in files)
+  {
+    var directoryPath = Path.Combine(destPath, day.Key.ToString("yyyyMMdd"));
+    Directory.CreateDirectory(directoryPath);
+
+    foreach (var (path, date) in day.Value)
+    {
+      var fileName = Path.GetFileName(path);
+      var newFilePath = Path.Combine(directoryPath, fileName);
+      File.Copy(path, newFilePath);
+    }
+  }
+}
 
 
-Dictionary<int, Dictionary<int, Dictionary<int, List<(string, DateTimeOffset)>>>> GetFilesSeparatedByDate(string parentDirectory)
+Dictionary<int, Dictionary<int, Dictionary<int, (string path, DateTimeOffset date)[]>>> GetFilesSeparatedByYearAndMonthAndDay(string parentDirectory)
 {
   var files = GetFiles(parentDirectory);
 
@@ -90,7 +154,16 @@ Dictionary<int, Dictionary<int, Dictionary<int, List<(string, DateTimeOffset)>>>
         .ToDictionary(monthGroup => monthGroup.Key,
           monthGroup => monthGroup.GroupBy(file => file.Value.Day)
             .ToDictionary(dayGroup => dayGroup.Key,
-              dayGroup => dayGroup.Select(file => (file.Key, file.Value)).ToList())));
+              dayGroup => dayGroup.Select(file => (file.Key, file.Value)).ToArray())));
+}
+Dictionary<DateOnly, (string path, DateTimeOffset date)[]> GetFilesSeparatedByDay(string parentDirectory)
+{
+  var files = GetFiles(parentDirectory);
+
+  return files
+    .GroupBy(file => new DateOnly(file.Value.Year, file.Value.Month, file.Value.Day))
+    .ToDictionary(dayGroup => dayGroup.Key,
+      dayGroup => dayGroup.Select(file => (file.Key, file.Value)).ToArray());
 }
 Dictionary<string, DateTimeOffset> GetFiles(string parentDirectory)
 {
@@ -124,9 +197,9 @@ DateTimeOffset GetShootingDate(string filePath)
   return date.Value;
 }
 
-
 enum SeparateMode
 {
-  ByDate,
   ByNone,
+  ByYearAndMonthAndDay,
+  ByDay,
 }
